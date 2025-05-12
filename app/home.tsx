6454f7b75375ac907ef904cf4bcc41df2e3fe408
Suyash Pradhan <pradhansuyash39@gmail.com>
2025-05-12 23:39:58 +0530

    script for bare minimum app setup

---
+++ app/home.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-4xl font-medium text-red-900">Hello World</Text>
+    </View>
+  );
+}
